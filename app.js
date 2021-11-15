247c517540e8fb48489eb312933860905a502fe3
igrimzyi <1isaiahofficial1@gmail.com>
2021-11-15 00:36:20 -0800

    Refactored Javascript
    Start working on buttons

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let billPrice = 0 ;
 const billInput = document.querySelector("#initialPrice"); 
+const tipInput = document.querySelectorAll(".buttonTip");
 
+const replaceAmount = document.getElementsByClassName("amountTipPerson");
+
+for(let input of tipInput){
+    input.addEventListener("click", function(){
+   console.log(input.value)
+    })
+}
 billInput.addEventListener("input", function(){
 billPrice = billInput.value;
+console.log(parseInt(billPrice));
 })
+
+function math(){
+    let price = parseInt(billPrice) + 20 ;
+    console.log(price);
+}
+
+
 
 
 
